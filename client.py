f74ee4d532a7d4c6b2beaf6bf50bea9f6b202cd9
osvaldo <aoga88@gmail.com>
2014-08-05 23:45:12 -0500

    v1

--- client.py
+++ client.py
@@ -2,8 +2,7 @@
 import psutil, json, requests, getpass, hashlib
 memory = psutil.virtual_memory()
 swap = psutil.swap_memory()
-disk = {'part': [], 'usage': {}}
-server_id = '2'
+disk = {'part': {}, 'usage': {}}
 noditor_url = 'http://192.168.0.2:8080'
 
 def configure_app():
@@ -23,7 +22,6 @@ def configure_app():
 	user = login.json()
 	
 	try:
-		#print user['_id']
 		user_id = user['_id']
 		serverObj = {
 			'user': user_id
@@ -49,48 +47,55 @@ def configure_app():
 	except KeyError:
 		print "The email or password are incorrect. Please try again"
 
-for index in psutil.disk_partitions():
-	disk['part'].append(index)
-	usage = psutil.disk_usage(index.device)
-	disk['usage'][index.device] = {
-		'total': usage[0],
-		'used': usage[1],
-		'free': usage[2],
-		'percent': usage[3]
-	}
 
-data = {'server_id': server_id,
-	'cpu': {
-		'count': psutil.cpu_count(False), 
-	 	'count_logical': psutil.cpu_count(True),
-	 	'per': psutil.cpu_percent(1, True)
-	},
-	'memory': {
-		'total': memory[0],
-		'avail': memory[1],
-		'per': memory[2],
-		'used': memory[3],
-		'free': memory[4]
-	},
-	'swap': {
-		'total': swap[0],
-		'used': swap[1],
-		'free': swap[2],
-		'per': swap[3],
-		'sin': swap[4],
-		'sout': swap[5]
-	},
-	'disk': disk
-	}
 
-#r = requests.post('https://10.110.103.52:8080/api/serverdata')
+def putData(configFile):
+	for index in psutil.disk_partitions():
+		disk['part'][index.device] = index
+		usage = psutil.disk_usage(index.device)
+		disk['usage'][index.device] = {
+			'total': usage[0],
+			'used': usage[1],
+			'free': usage[2],
+			'percent': usage[3]
+		}
 
+	data = {
+		'cpu': {
+			'count': psutil.cpu_count(False), 
+	 		'count_logical': psutil.cpu_count(True),
+		 	'per': psutil.cpu_percent(1, True)
+		},
+		'memory': {
+			'total': memory[0],
+			'avail': memory[1],
+			'per': memory[2],
+			'used': memory[3],
+			'free': memory[4]
+		},
+		'swap': {
+			'total': swap[0],
+			'used': swap[1],
+			'free': swap[2],
+			'per': swap[3],
+			'sin': swap[4],
+			'sout': swap[5]
+		},
+		'disk': disk,
+		'users': psutil.users(),
+		'boot_time': psutil.boot_time()
+		}
+	configStr  = configFile.read()
+        config = json.JSONDecoder().decode(configStr)
+        data['server_id'] = config['server_id']
+        serverdata = json.dumps(data)
+        headers = {'content-type': 'application/json'}
+        r = requests.put(noditor_url + '/api/serverdata', data=serverdata, auth=(config['user'],config['password']), headers=headers)
 try:
 	configFile = open('noditor.conf', 'r')
-	configStr  = configFile.read()
-	config = json.JSONDecoder().decode(configStr)
-	print config['server_id']
+	putData(configFile)
 except IOError:
 	configure_app()
+	configFile = open('noditor.conf', 'r')
+	putData(configFile)
 
-#print json.JSONEncoder().encode(data)
---
+++ service.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while true
+do 
+    ./client.py
+    sleep 5
+done
